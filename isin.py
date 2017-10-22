cc3266b6cb97b8753d0bb4b3434a9ea895cc3dbb
Wei Shao Yang <ediryyang@gmail.com>
2017-10-22 23:42:13 +0800

    Update isin.py

--- isin.py
+++ isin.py
@@ -8,21 +8,21 @@ def isIn(char, aStr):
     # bisection search
     cp = int(len(aStr)/2) # initial check point
     if char == aStr[cp]:
-        # print(aStr[cp])
-        # print("1")
+        # print(aStr[cp])                        # for debug
+        # print("1")                             # for debug
         return True
     elif char < aStr[0] or char > aStr[-1]:
-        # print(aStr[0], aStr[-1])
-        # print("2")
+        # print(aStr[0], aStr[-1])               # for debug
+        # print("2")                             # for debug
         return False
     elif char < aStr[cp]:
         return isIn(char, aStr[:cp])
-        # print(aStr[:cp])
+        # print(aStr[:cp])                       # for debug
     else: # char > aStr[cp]
         return isIn(char, aStr[cp:])
-        # print(aStr[:cp])
+        # print(aStr[:cp])                       # for debug
 
-c = 'b'
+c = 'b'        # can change c and s for test
 s = 'abcdef'
 if isIn(c,s):
     print("found")
